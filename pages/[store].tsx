fe21b28bc9c187a10a3fdb33e5c113a4298f7974
Matheus Cunha <matheus@iglu.com.br>
2025-12-22 12:34:34 -0300

    Add Riomar Fortaleza

--- pages/[store].tsx
+++ pages/[store].tsx
@@ -39,6 +39,7 @@ const Trocas = {
   patiopaulista: 7238528638,
   vilamariana: 4702915515,
   florianopolis: 4704865322,
+  riomarfortaleza: 7860688070,
 };
 
 const Td = ({ children }) => <td style={{ border: "1px solid black", padding: "6px" }}>{children}</td>
